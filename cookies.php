c0e568868ef94897e2a40185cdbaa2aa20cb9687
Vishnu Kant Maurya <vishnukant0077@gmail.com>
2021-05-02 19:22:20 +0530

    basic of php

---
+++ cookies.php
@@ -0,0 +1,8 @@
+<?php
+   setcookie("name", "Testing Data", time()+3600, "/","", 0);
+   setcookie("age", "36", time()+3600, "/", "",  0);
+   
+	echo '<pre>';
+	//print_r($_COOKIE);
+         
+?>
---
+++ functions/argument.php
@@ -0,0 +1,10 @@
+<?php
+ /* Defining a PHP Function */
+ function addFunction($num1, $num2) {
+	$sum = $num1 + $num2;
+	echo "Sum of the two numbers is : $sum";
+ }
+
+
+ addFunction(10, 20);
+?>
